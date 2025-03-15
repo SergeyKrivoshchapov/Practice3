dc5130ea9f22e30aaa1bf9b0226d137bbb8d2ea2
SergeyKrivoshchapov <krivoshchapovsergey@gmail.com>
2025-03-16 03:57:12 +0700

    changed main func to create struct not dynamically, get to funcs by value. Changed list struct

--- main.c
+++ main.c
@@ -2,30 +2,28 @@
 #include "list.h"
 int main()
 {
-	// Инициализация в динамической памяти трёх экземпляров структуры
+	// Инициализация трёх экземпляров структуры
 	printf("Creating first struct.\n");
-	WIFI* router1 = dynamic_struct_create("1", 1, Is5G);
+	WIFI router1;
 
-	if (router1 == NULL) return 1; // Проверка корректности инициализации экземпляра
+	if (struct_init_result(init_struct(&router1, "1", 1, Is5G)) != 0) return 1;
 
 	printf("First: \n");
-	print_struct(router1); // Вывод сведений о структуре
+	print_struct(&router1); // Вывод сведений о структуре
 
 	printf("Creating second struct.\n");
-	WIFI* router2 = dynamic_struct_create("2", 2, Is5G);
+	WIFI router2;
+	if (struct_init_result(init_struct(&router2, "2", 2, Is5G)) != 0) return 1;
 
-	if (router2 == NULL) return 1;
-	
 	printf("Second: \n");
-	print_struct(router2);
+	print_struct(&router2);
 
 	printf("Creating third struct.\n");
-	WIFI* router3 = dynamic_struct_create("3", 3, Not5G);
-	
-	if (router3 == NULL) return 1;
+	WIFI router3;
+	if (struct_init_result(init_struct(&router3, "3", 3, Not5G)) != 0) return 1;
 
 	printf("Third: \n");
-	print_struct(router3);
+	print_struct(&router3);
 
 	// Инициализация неупорядоченного списка 
 	printf("Initializing not ordered list with three structs.\n");
@@ -38,9 +36,9 @@ int main()
 	router_list->head = NULL;
 	
 	// Добавляем элементы в начало списка
-	if (push_front(router_list, *router1) == 1) return 1;
-	if (push_front(router_list, *router2) == 1) return 1;
-	if (push_front(router_list, *router3) == 1) return 1;
+	if (push_front(router_list, router1) == 1) return 1;
+	if (push_front(router_list, router2) == 1) return 1;
+	if (push_front(router_list, router3) == 1) return 1;
 
 	print_list(router_list); // Вывод сведений о  каждом элементе списка
 
@@ -50,61 +48,29 @@ int main()
 	print_list(router_list);
 
 	delete_list(router_list); // Удаление списка полностью.
-	
-	//Освобождение динамической памяти, выделенной под экземпляры структуры
-	dynamic_struct_free(router1);
-	dynamic_struct_free(router2);
-	dynamic_struct_free(router3);
-	
-	printf("Creating first dynamic struct.\n");
-	router1 = dynamic_struct_create("1", 1, Is5G);
+		
+	printf("Creating fourth struct.\n");
+	WIFI router4;
+	if (struct_init_result(init_struct(&router4, "4", 4, Not5G)) != 0) return 1;
 
-	if (router1 == NULL) return 1;
+	printf("Fourth: \n");
+	print_struct(&router1);
 
-	printf("First: \n");
-	print_struct(router1);
-
-	printf("Creating second dynamic struct.\n");
-	router2 = dynamic_struct_create("2", 2, Is5G);
-
-	if (router2 == NULL) return 1;
-	
-	printf("Second: \n");
-	print_struct(router2);
-
-	printf("Creating third dynamic struct.\n");
-	router3 = dynamic_struct_create("3", 3, Not5G);
-	
-	if (router3 == NULL) return 1;
-
-	printf("Third: \n");
-	print_struct(router3);
-
-	printf("Creating fourth dynamic struct.\n");
-	WIFI* router4 = dynamic_struct_create("4", 3, Is5G);
-
-	if (router4 == NULL) return 1;
-	
 	printf("Initializing ordered list\n");
 
-	if(add_to_ordered(router_list, *router1) == 1) return 1;
-	if(add_to_ordered(router_list, *router2) == 1) return 1;
-	if(add_to_ordered(router_list, *router3) == 1) return 1;
-	if(add_to_ordered(router_list, *router4) == 1) return 1;
+	if(add_to_ordered(router_list, router1) == 1) return 1;
+	if(add_to_ordered(router_list, router2) == 1) return 1;
+	if(add_to_ordered(router_list, router3) == 1) return 1;
+	if(add_to_ordered(router_list, router4) == 1) return 1;
 
 	print_list(router_list);
 
-	delete_selected_element(router_list, *router4);
+	delete_selected_element(router_list, router4);
 
 	printf("Ordered list after deleting element:\n");
 	print_list(router_list);
 
 	delete_list(router_list);
-
-	dynamic_struct_free(router1);
-	dynamic_struct_free(router2);
-	dynamic_struct_free(router3);
-	dynamic_struct_free(router4);
 
 	return 0;
 }
